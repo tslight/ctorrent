596fd63b57c4d9822f20346e3edf4d36bcce917e
tslight <tslight@pm.com>
2021-10-21 11:54:47 +0100

    add options for different search engines

--- ctorrent/__main__.py
+++ ctorrent/__main__.py
@@ -7,14 +7,27 @@ from .torrents import TPB, Solid
 def get_args():
     parser = ArgumentParser(description="search for torrents.")
     parser.add_argument("query", help="search query.")
+    parser.add_argument(
+        "-s", "--site",
+        choices=["all", "tpb", "solid"],
+        default="all",
+        nargs="?",
+        help="torrent website to search",
+    )
     return parser.parse_args()
 
 
 def main():
     args = get_args()
-    tpb_torrents = TPB().search(args.query)
-    solid_torrents = Solid().search(args.query)
-    torrents = tpb_torrents + solid_torrents
+    if args.site == "all":
+        tpb_torrents = TPB().search(args.query)
+        solid_torrents = Solid().search(args.query)
+        torrents = tpb_torrents + solid_torrents
+    elif args.site == "tpb":
+        torrents = TPB().search(args.query)
+    elif args.site == "solid":
+        torrents = Solid().search(args.query)
+
     columns = ["Name", "Size", "Seeders", "Leechers", "Site"]
     print(show_table(torrents, columns))
 
