4fdb67d2c223fdc5da8f301b64b42596ab2b487a
tslight <tslight@pm.com>
2021-10-31 13:49:54 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,33 @@
 # Copyright (c) 2021, Toby Slight. All rights reserved.
 # ISC License (ISCL) - see LICENSE file for details.
 
-import setuptools
+from setuptools import setup, find_packages
+import subprocess
+
+
+def get_latest_tag():
+    try:
+        cmd_output = subprocess.run(
+            ["git", "describe", "--tags", "--abbrev=0"], stdout=subprocess.PIPE
+        )
+        return cmd_output.stdout.strip().decode("utf-8")
+    except EnvironmentError:
+        print("Couldn't run git to get a version number for setup.py")
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="ctorrent",
-    version="0.0.1",
+    version=get_latest_tag(),
     author="Toby Slight",
     author_email="tslight@pm.me",
     description="Curses Torrent Browser",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tslight/ctorrent",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: ISC License (ISCL)",
@@ -23,8 +35,8 @@ setuptools.setup(
     ),
     install_requires=["requests", "ctable"],
     entry_points={
-        'console_scripts': [
-            'ctorrent = ctorrent.__main__:main',
+        "console_scripts": [
+            "ctorrent = ctorrent.__main__:main",
         ],
-    }
+    },
 )
